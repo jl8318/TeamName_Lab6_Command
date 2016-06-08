65f670229cbe4e139733498d28ae67882434c068
Jixiang Lu <jl8318@ship.edu>
2016-06-08 01:31:49 -0400

    AttackCommand need the turn() and move() method

---
+++ test/ui/command/TestAttackCommand.java
@@ -0,0 +1,22 @@
+package ui.command;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test the AttackCommand class.
+ * 
+ * @author Jixiang Lu
+ *
+ */
+public class TestAttackCommand
+{
+
+	@Test
+	public void testInitialization()
+	{
+		fail("Not yet implemented");
+	}
+
+}
